bad888d3060ec6f3d8cec1473fa34f70d6ea174c
mburu <mburumaina648@gmail.com>
2024-11-27 15:59:59 +0300

    fixed error logs

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,31 +1,29 @@
-import {ImageResponse} from 'next/og'
+import { ImageResponse } from 'next/og';
 
-export const runtime = 'edge'
+export const runtime = 'edge';
 
 // Image metadata
-export const alt = 'blogs homepage'
+export const alt = 'blogs homepage';
 export const size = {
-   width: 1200,
-   height: 630
-}
+  width: 1200,
+  height: 630,
+};
 
-export const contentType = 'image/png'
+export const contentType = 'image/png';
 
-export default async function Image(){
-   // Font
+export default async function Image() {
+  // Font
 
-   return new ImageResponse(
-      <div tw='text-md font-bold bg-white text-black w-full h-full flex items-center justify-center'>
-         <h4>
-            Blogs Examples Test
-         </h4>
-
-      </div>
-   ),
-   {
+  return (
+    new ImageResponse(
+      (
+        <div tw="text-md font-bold bg-white text-black w-full h-full flex items-center justify-center">
+          <h4>Blogs Examples Test</h4>
+        </div>
+      )
+    ),
+    {
       ...size,
-
-   }
-
-
+    }
+  );
 }
